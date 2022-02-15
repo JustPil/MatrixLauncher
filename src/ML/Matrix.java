794b08262ca2a05ca5ffc43d66d47bc089054e58
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 22:10:28 -0800

    Minor style changes

--- src/ML/Matrix.java
+++ src/ML/Matrix.java
@@ -2,24 +2,21 @@ package ML;
 
 import java.util.Random;
 
-public class Matrix
-{
+public class Matrix {
     private int[][] matrix;
     private Random random = new Random();
 
     /**
      * Constructor calls the matrixReset method to create a matrix array.
      */
-    public Matrix()
-    {
+    public Matrix() {
         matrixReset();
     }
 
     /**
      * matrixReset Sets the matrix variable to a 3 x 3 matrix of pseudorandom integers from 1 to 3.
      */
-    public void matrixReset()
-    {
+    public void matrixReset() {
         matrix = new int[][]{{random.nextInt(3) + 1, random.nextInt(3) + 1, random.nextInt(3) + 1},
                 {random.nextInt(3) + 1, random.nextInt(3) + 1, random.nextInt(3) + 1},
                 {random.nextInt(3) + 1, random.nextInt(3) + 1, random.nextInt(3) + 1}};
@@ -31,14 +28,10 @@ public class Matrix
      * @param i The index row.
      * @param j The index column.
      */
-    public void launch(int i, int j)
-    {
-        if(i >= matrix.length || i < 0 || j >= matrix[0].length || j < 0)
-        {
-            System.out.println("Indices are out of bounds.");
-        }
-        else
-        {
+    public void launch(int i, int j) {
+        if(i >= matrix.length || i < 0 || j >= matrix[0].length || j < 0) {
+            System.out.println("Indexes are out of bounds.");
+        } else {
             System.out.println("Initial matrix:");
             display();
             System.out.println("You launch the catapult at row " + i + " column " + j + "!\nPOP!");
@@ -55,22 +48,17 @@ public class Matrix
      * @param i The matrix row.
      * @param j The matrix column.
      */
-    private void pop(int i, int j)
-    {
-        if(i + 1 < matrix.length && matrix[i + 1][j] == matrix[i][j])
-        {
+    private void pop(int i, int j) {
+        if(i + 1 < matrix.length && matrix[i + 1][j] == matrix[i][j]) {
             matrix[i + 1][j] = 0;
         }
-        if(i - 1 >= 0 && matrix[i - 1][j] == matrix[i][j])
-        {
+        if(i - 1 >= 0 && matrix[i - 1][j] == matrix[i][j]) {
             matrix[i - 1][j] = 0;
         }
-        if(j + 1 < matrix[i].length && matrix[i][j + 1] == matrix[i][j])
-        {
+        if(j + 1 < matrix[i].length && matrix[i][j + 1] == matrix[i][j]) {
             matrix[i][j + 1] = 0;
         }
-        if(j - 1 >= 0 && matrix[i][j - 1] == matrix[i][j])
-        {
+        if(j - 1 >= 0 && matrix[i][j - 1] == matrix[i][j]) {
             matrix[i][j - 1] = 0;
         }
         matrix[i][j] = 0;
@@ -79,18 +67,14 @@ public class Matrix
     /**
      * shift Swaps matrix values such that zeroes will shift above nonzero integers, if applicable.
      */
-    private void shift()
-    {
-        for(int j = 0; j < matrix[0].length; j++)
-        {
-            if(matrix[2][j] == 0 && matrix[1][j] == 0 && matrix[0][j] != 0)
-            {
+    private void shift() {
+        for(int j = 0; j < matrix[0].length; j++) {
+            if(matrix[2][j] == 0 && matrix[1][j] == 0 && matrix[0][j] != 0) {
                 int swap = matrix[0][j];
                 matrix[0][j] = 0;
                 matrix[2][j] = swap;
             }
-            if(matrix[2][j] == 0 && matrix[1][j] != 0)
-            {
+            if(matrix[2][j] == 0 && matrix[1][j] != 0) {
                 int swap = matrix[1][j];
                 matrix[1][j] = 0;
                 matrix[2][j] = swap;
@@ -98,8 +82,7 @@ public class Matrix
                 matrix[0][j] = 0;
                 matrix[1][j] = swap;
             }
-            if(matrix[1][j] == 0 && matrix[0][j] != 0 && matrix[2][j] != 0)
-            {
+            if(matrix[1][j] == 0 && matrix[0][j] != 0 && matrix[2][j] != 0) {
                 int swap = matrix[0][j];
                 matrix[0][j] = 0;
                 matrix[1][j] = swap;
@@ -110,13 +93,10 @@ public class Matrix
     /**
      * display Outputs a display of the current matrix.
      */
-    private void display()
-    {
-        for(int i = 0; i < matrix.length; i++)
-        {
-            for(int j = 0; j < matrix[0].length; j++)
-            {
-                System.out.print(matrix[i][j] + " ");
+    private void display() {
+        for(int[] ints : matrix) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                System.out.print(ints[j] + " ");
             }
             System.out.println();
         }
